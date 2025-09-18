75e8037930c61f4ee7f26454e5f12ee1f71f5bd1
Taran Mittal <taranmittal09@gmail.com>
2025-09-18 22:51:46 +1000

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome to Hack Caps!`}
+      description="YSWS where you make your own custom Keyboard Keycaps!">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
