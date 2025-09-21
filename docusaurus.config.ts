feb1e83527199a73c4f6bac0df3403ac873da782
Taran Mittal <taranmittal09@gmail.com>
2025-09-21 12:23:14 +1000

    bleh

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,7 +53,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/preview.jpg',
     navbar: {
       title: 'Hack Caps', // title replace with your site name
       logo: {
